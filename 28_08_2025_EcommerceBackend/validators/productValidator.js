a7fa4caeb46a43c25fb8ce51fe732fe61896f382
shivramsinghgurjar <shivramsinghgurjar2@gmail.com>
2025-08-28 12:25:32 +0530

    Ecommerce-backend

---
+++ 28_08_2025_EcommerceBackend/validators/productValidator.js
@@ -0,0 +1,5 @@
+import { body } from "express-validator"; 
+export const productValidator = [ 
+body("name").notEmpty().withMessage("Product name is required"), 
+body("price").isNumeric().withMessage("Price must be a number") 
+];
