dec5af5e817e876e949899eb88368e5f18d9f69e
shivramsinghgurjar <shivramsinghgurjar2@gmail.com>
2025-09-05 22:01:53 +0530

    Add Mongodb Uri from Compass to cloud

--- 28_08_2025_EcommerceBackend/config/db.js
+++ 28_08_2025_EcommerceBackend/config/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    // await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(`${process.env.MONGO_URI}/ecommerece`);
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err);
