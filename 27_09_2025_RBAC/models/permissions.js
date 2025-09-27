d98a583f774a3e0ae253cf117321a3d993608d74
shivramsinghgurjar <shivramsinghgurjar2@gmail.com>
2025-09-27 12:20:18 +0530

    RBAC_Project

---
+++ 27_09_2025_RBAC/models/permissions.js
@@ -0,0 +1,13 @@
+// models/permissions.js
+
+class Permissions {
+  constructor() {
+    this.permissions = [];
+  }
+
+getPermissionsByRoleName(roleName) {
+    const role = roles.roles.find((r) => r.name === roleName);
+    return role ? role.permissions : [];
+  }
+}
+module.exports = Permissions;
---
+++ 27_09_2025_RBAC/models/user.js
@@ -0,0 +1,16 @@
+// models/user.js
+
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  role: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
